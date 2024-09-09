c813c29115f153da40732fcc5081f3f25b4c4f39
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-09-09 15:54:49 +0100

    credit search page

---
+++ src/app/credit-search/page.tsx
@@ -0,0 +1,58 @@
+import { Button } from '@/components/shared/Button';
+import { Section } from '@/components/shared/Section';
+import { SectionTitle } from '@/components/shared/SectionTitle';
+import { ButtonContentFields } from '@/util/models';
+import { convertWysywyg } from '@/util/utilFunctions';
+
+type CreditSearchContent = {
+  button: ButtonContentFields;
+  page_content: string;
+  page_title: string;
+  subheading: string;
+};
+
+async function fetchCreditSearchContent() {
+  const res = await fetch(`${process.env.HOST_URL}/credit-search/api`, {
+    // next: {
+    //   revalidate: 10,
+    // },
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function CreditSearchPage() {
+  const data = await fetchCreditSearchContent();
+
+  const content: CreditSearchContent = data.acf;
+
+  const bodyContent = convertWysywyg(content.page_content);
+
+  return (
+    <Section
+      type='narrow'
+      classes='bg-chequered-bg bg-cover bg-bottom flex flex-col items-center'
+    >
+      <SectionTitle
+        title={content.page_title}
+        textColour='ash'
+        lineColour='mediumblue'
+        alignment='centred'
+      />
+      <h3 className='font-semibold text-center my-4'>{content.subheading}</h3>
+      <div
+        className='w-2/3 my-16'
+        dangerouslySetInnerHTML={{ __html: bodyContent }}
+      />
+      <Button
+        colour='mediumblue'
+        title={content.button.title}
+        url={content.button.url}
+        target={content.button.target}
+      />
+    </Section>
+  );
+}
