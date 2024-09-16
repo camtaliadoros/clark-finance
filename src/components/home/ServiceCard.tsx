171aeb1ba51f8d16affaed0a6a7bf4bf5380f2fd
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-09-16 21:54:17 +0100

    fix a underline

--- src/components/home/ServiceCard.tsx
+++ src/components/home/ServiceCard.tsx
@@ -16,7 +16,10 @@ export const ServiceCard = ({
   order,
 }: ServiceCardProps) => {
   return (
-    <Link className='relative h-52 group ' href={`services/${slug}`}>
+    <Link
+      className='relative h-52 group no-underline'
+      href={`services/${slug}`}
+    >
       <div className='bg-lightgrey1 relative px-4 py-6 h-full space-y-4 rounded-br-[120px] z-10 transition-all group-hover:bg-lightgrey2 group-hover:rounded-br-[140px] '>
         <h2 className='font-semibold text-xl h-1/3 w-2/3 leading-tight '>
           {title}
