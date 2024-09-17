915214dae0c9016834ebd003a0fa2c238d45c963
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-09-17 22:52:58 +0100

    styling chnage

--- src/components/aboutUs/WhyChooseUsCard.tsx
+++ src/components/aboutUs/WhyChooseUsCard.tsx
@@ -9,7 +9,7 @@ export const WhyChooseUsCard = ({ content }: WhyChooseUsCardProps) => {
     <div className='grid grid-cols-5 w-96 bg-lightblue bg-opacity-25 rounded p-12 hover:bg-opacity-50 transition'>
       <div className=''>icon goes here</div>
       <div className='col-span-4 flex flex-col'>
-        <h5 className='text-chalk font-semibold text-3xl'>
+        <h5 className='text-chalk font-semibold text-2xl'>
           {content.why_choose_us_item_title}
         </h5>
         <div className='flex flex-col gap-2'>
