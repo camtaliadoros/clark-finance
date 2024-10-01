3962fae951209671acac04999a25975408a8a50e
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-10-01 17:54:16 +0100

    hero banner styling fix

--- src/components/home/ServiceCards.tsx
+++ src/components/home/ServiceCards.tsx
@@ -37,7 +37,7 @@ export const ServiceCards = async ({ classes }: PageProps) => {
 
   return (
     <Section type='wide' classes={`${classes}  py-0`}>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 -mt-40'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 -mt-48'>
         {services.map((service) => (
           <ServiceCard
             key={service.acf.homepage_order}
