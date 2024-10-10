c0947a8c30d16548c447bb201fecfc9d6750dce2
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-10-10 23:14:49 +0100

    not found styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,12 @@
+import { Button } from '@/components/shared/Button';
 import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href='/'>Return Home</Link>
+    <div className='flex flex-col justify-center items-center py-12 space-y-8'>
+      <h2 className='mb-0'>Oops...</h2>
+      <p>This page doesn't exist</p>
+      <Button colour='mediumblue' title='Return Home' url='/' />
     </div>
   );
 }
