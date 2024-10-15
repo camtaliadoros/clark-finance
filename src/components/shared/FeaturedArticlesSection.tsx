85ca8fc2884240f741d9b5d131449f249b969a25
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-10-15 17:10:09 +0100

    articles styling change

--- src/components/shared/FeaturedArticlesSection.tsx
+++ src/components/shared/FeaturedArticlesSection.tsx
@@ -45,6 +45,7 @@ export const FeaturedArticlesSection = async ({
         lineColour='mediumblue'
         textColour={`${bgColour === 'dark' ? 'chalk' : 'ash'}`}
         alignment='centred'
+        classes='2xl:mb-12'
       />
       <div className='flex flex-col items-center  space-y-12 md:space-y-0 md:grid md:grid-cols-2 md:grid-rows-1 lg:grid-cols-3 md:items-start w-full'>
         {featuredContent.map((articleData) => (
