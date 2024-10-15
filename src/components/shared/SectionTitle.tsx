11ea931d25a8517919be3ffb88c12c0859f08249
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-10-15 16:15:49 +0100

    section title styling chnages

--- src/components/shared/SectionTitle.tsx
+++ src/components/shared/SectionTitle.tsx
@@ -15,14 +15,14 @@ export const SectionTitle = ({
 }: SectionTitleProps) => {
   return (
     <div
-      className={` ${classes} flex flex-col space-y-3 ${
+      className={` ${classes} flex flex-col space-y-3 2xl:space-y-6 ${
         alignment === 'centred' ? 'items-center' : 'items-start'
       } `}
     >
       <h2
-        className={`${alignment === 'centred' ? 'text-4xl' : 'text-2xl'} ${
-          textColour === 'ash' ? 'text-ash' : 'text-chalk'
-        } text-center`}
+        className={`${
+          alignment === 'centred' ? 'text-4xl 2xl:text-5xl' : 'text-2xl'
+        } ${textColour === 'ash' ? 'text-ash' : 'text-chalk'} text-center`}
       >
         {title}
       </h2>
