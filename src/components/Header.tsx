f3cd2205e9ca8009d63f5b387ab3643c35a8f3d3
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-10-28 11:52:11 +0000

    header styling fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import { PhoneNumberWithIcon } from './shared/PhoneNumberWithIcon';
 
 export const Header = () => {
   return (
-    <header className='flex justify-between items-center  h-16 md:h-24 bg-chalk px-4 md:px-10 drop-shadow-md shrink-0 w-full z-50'>
+    <header className='flex justify-between items-center  h-16 md:h-24 bg-chalk px-4 md:px-10 drop-shadow-md shrink-0 w-full z-50 space-x-4'>
       <Link href='/' prefetch={true}>
         <Image
           src='/images/clark-finance-logo.png'
@@ -17,13 +17,13 @@ export const Header = () => {
           priority={true}
         />
       </Link>
-      <div className='flex space-x-8'>
-        <div className='flex space-x-4 items-center'>
+      <div className='flex  space-x-2 md:space-x-8'>
+        <div className='flex space-x-2 md:space-x-4 items-center '>
           <p className='font-bold text-mediumblue mb-0'>Get in touch:</p>
           <EmailWithIcon colour='dark' hideOnMobile={true} />
           <PhoneNumberWithIcon colour='dark' size='sm' hideOnMobile={true} />
         </div>
-        <div>
+        <div className='flex justify-center items-center'>
           <Menu />
         </div>
       </div>
