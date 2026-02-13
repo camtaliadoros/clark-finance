f20e090a2aca9e2ca791843ea76d712192954e35
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2026-02-13 21:33:59 +0000

    li font size fix

--- src/components/home/HeroBanner.tsx
+++ src/components/home/HeroBanner.tsx
@@ -18,12 +18,12 @@ export const HeroBanner = async ({
     <>
       <div className='w-full'>
         <div
-          className='bg-hero-image bg-bottom md:bg-center bg-cover lg:bg-fixed z-0  flex flex-col justify-center px-4 md:px-12 md:px-18 lg:px-24 2xl:px-60 space-y-1'
+          className='bg-hero-image bg-bottom md:bg-center bg-cover lg:bg-fixed z-0  flex flex-col justify-center px-4 md:px-12 md:px-18 lg:px-24 2xl:px-60'
           style={{ height: '32rem' }}
         >
           <h2 className='text-chalk font-normal'>{subtitle}</h2>
           <hr className='border-chalk border-t-2 w-20' />
-          <h1 className='text-chalk font-semibold '>{title}</h1>
+          <h1 className='text-chalk font-semibold mt-2 '>{title}</h1>
           <div className='flex flex-col w-2/3 md:flex-row pt-9 space-y-4 md:space-x-6 md:space-y-0'>
             <Button url={cta1.url} title={cta1.title} colour='chalk' />
             <Button url={cta2.url} title={cta2.title} colour='chalk' />
